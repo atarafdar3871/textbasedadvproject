5a6e6f6712e7c2781794291cd04b5c6add13e79b
Xtentacles <BT_1N3_18@BT_1N3_18.bktech.local>
2017-11-01 13:15:32 -0400

    @slumpgod hie

--- TBAStarterCode/src/game/Person.java
+++ TBAStarterCode/src/game/Person.java
@@ -1,6 +1,11 @@
 package game;
-
+import items.Item;
 public abstract class Person {
+	
+	private int healthPoints; //used for enemies and player during fighting to know when someone is dead or not, can possibly change item drops from enemies based on health
+	private String characterName; //most likely going to be general names like Bat or Goblin or whatever
+	private Item inventory[]; //inventory should be linked to each person and when dead you drop your inventory and the player can choose to pick it up
+	private boolean living; //if healthPoints <= 0, person is dead, if this happens to you, GAME OVER 
 	
 	public Person(int healthPoints, String characterName, Item inventory[], boolean living) {
 		this.healthPoints = healthPoints;
@@ -9,6 +14,13 @@ public abstract class Person {
 		this.living = true;
 	}
 	
-	
+	//the player can check inventory by typing in things like "check inventory" or whatever. it should return as a string into the console
+	public String getInventory(Item inventory[]) {
+		String textInv = "You currently are carrying ";
+		for (int i=1)
+		
+		
+		return textInv
+	}
 	
 }
--- TBAStarterCode/src/items/Item.java
+++ TBAStarterCode/src/items/Item.java
@@ -2,10 +2,13 @@ package items;
 
 public abstract class Item {
 	
+	private String itemName;
+	private boolean consumable;
+	
 	public Item(String itemName, boolean consumable) {
 		this.itemName = itemName;
-		this.consumable = consumable;
-		
+		this.consumable = consumable;		
 	}
 	
+//	public 
 }
